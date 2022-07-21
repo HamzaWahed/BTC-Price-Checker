e009ac9f3f56c4da87fed4445e0e83723e535583
Hamza Wahed <hamzawahed@dal.ca>
2022-07-21 20:30:25 -0300

    add price in different currencies

--- btc-price-checker/routes/index.tsx
+++ btc-price-checker/routes/index.tsx
@@ -1,7 +1,7 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-
+import { Handlers, PageProps } from "$fresh/server.ts"
 const url: string = "https://api.coindesk.com/v1/bpi/currentprice.json";
 
 export interface Price {
@@ -47,8 +47,21 @@ export interface Eur {
   rate_float: number
 }
 
+export const handler: Handlers<Price | null> = {
+  async GET(_,ctx){
+    const resp = await fetch(url);
+    if(resp.status===200){
+      const price:Price = await resp.json();
+      return ctx.render(price);
+    }
+    return ctx.render(null);
+  }
+}
 
-export default function Home() {
+export default function Home({data}: PageProps<Price|null>) {
+  if(!data){
+    return <h1>Data is not available</h1>
+  }
   return (
     <div class={tw`w-screen h-screen bg-gray-900`}>
       <div class={tw`p-8 mx-auto max-w-screen-md`}>
@@ -60,6 +73,18 @@ export default function Home() {
         />
         <p class={tw`my-10 text(center 3xl white)`}>
           Bitcoin Price Checker
+        </p>
+        <p class={tw`my-10 text(center 2xl white)`}>
+          USD: {data.bpi.USD.rate}
+        </p>
+        <p class={tw`my-10 text(center 2xl white)`}>
+          EUR: {data.bpi.EUR.rate}
+        </p>
+        <p class={tw`my-10 text(center 2xl white)`}>
+          GBP: {data.bpi.GBP.rate}
+        </p>
+        <p class={tw`my-10 text(center md white)`}>
+          Last Fetched at {data.time.updated}
         </p>
       </div>
     </div>
