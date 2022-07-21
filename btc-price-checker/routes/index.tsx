6180121f58f7dcbdab0e051482e79c2ff0516c99
Hamza Wahed <hamzawahed@dal.ca>
2022-07-21 20:23:53 -0300

    add logo and price

--- btc-price-checker/routes/index.tsx
+++ btc-price-checker/routes/index.tsx
@@ -1,21 +1,67 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
+
+const url: string = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+export interface Price {
+  time: Time
+  disclaimer: string
+  chartName: string
+  bpi: Bpi
+}
+
+export interface Time {
+  updated: string
+  updatedISO: string
+  updateduk: string
+}
+
+export interface Bpi {
+  USD: Usd
+  GBP: Gbp
+  EUR: Eur
+}
+
+export interface Usd {
+  code: string
+  symbol: string
+  rate: string
+  description: string
+  rate_float: number
+}
+
+export interface Gbp {
+  code: string
+  symbol: string
+  rate: string
+  description: string
+  rate_float: number
+}
+
+export interface Eur {
+  code: string
+  symbol: string
+  rate: string
+  description: string
+  rate_float: number
+}
+
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p class={tw`my-6`}>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
+    <div class={tw`w-screen h-screen bg-gray-900`}>
+      <div class={tw`p-8 mx-auto max-w-screen-md`}>
+        <img
+          src="/btc.png"
+          width="200px"
+          class="mx-auto"
+          alt=""
+        />
+        <p class={tw`my-10 text(center 3xl white)`}>
+          Bitcoin Price Checker
+        </p>
+      </div>
     </div>
   );
 }
